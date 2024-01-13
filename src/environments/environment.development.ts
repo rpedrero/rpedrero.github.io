ac0dcddd9ae70beaec779db5108fb0d90069c790
romain.pedrero <romain.pedrero@mines-ales.org>
2024-01-13 17:13:48 +0100

    Adapted configuration a little for deployment on GitHub Pages.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   localStorage: true, //True if data should be accessed and saved though a local storage (mock) rather than using a REST API.
-  apiUrl: 'http://localhost:8080/api/'
+  apiUrl: '' //If using a REST CRUD, must contain the URL of the API server.
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  localStorage: false,
-  apiUrl: ''
+  localStorage: true, //True if data should be accessed and saved though a local storage (mock) rather than using a REST API.
+  apiUrl: '' //If using a REST CRUD, must contain the URL of the API server.
 };
